0f9a78a135b0c439b9df1c52db549f07a79d1d49
Siavash Nezhad <siavash.nezhad@revature.net>
2021-09-23 20:09:56 -0500

    Finished customers

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -5,34 +5,24 @@ namespace Models
 {
     public class Customer
     {
-        public Customer() { }
+        public Customer() {}
 
-        //Constructor overloading (this is an example of polymorphism)
-        //The constructor behaves differently!
-        //depending on what is passed in
-        public Customer(string Company) : this()
+        public Customer(string name) : this ()
         {
-            this.Company = company;
+            this.Name = name;
         }
 
-        //constructor chaining
-        public Customer(string Company, string State) : this(customer)
+        public Customer(string name, string address) : this(name)
         {
-            this.State = state;
+            this.Address = address;
         }
 
-        public Customer(string Company, string State, string Email) : this(name, address) : this(name, state)
-        {
-            this.Email = email;
-        }
-
-        //Property
-        public string CustomerID { get; set; }
-        public string Company { get; set; }
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
         public string Email { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
-        public string State { get; set; }
+        public string Address { get; set; }
         public List<Order> Orders { get; set; }
     }
 }
