30283252d18fa6ef7772692079901db7049d13eb
Siavash Nezhad <siavash.nezhad@revature.net>
2021-09-25 21:52:54 -0500

    connected menu to db

---
+++ DL/DBRepo.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Model = Models;
+using Entity = DL.Entities;
+using Models;
+
+namespace DL
+{
+    public class DBRepo : IRepo
+    {
+        private Entity.siadbContext _context;
+        public DBRepo(Entity.siadbContext context)
+        {
+            _context = context;
+        }
+        public Model.Customer AddCustomer(Model.Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Model.Customer> GetAllCustomers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Model.Customer UpdateCustomer(Model.Customer customerToUpdate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- UI/MenuFactory.cs
+++ UI/MenuFactory.cs
@@ -1,12 +1,34 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using DL;
+using BL;
+using DL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
 
 namespace UI
 {
     public class MenuFactory
     {
-        
+        public static IMenu GetMenu(string menuString)
+        {
+            string connectionString = File.ReadAllText(@"../connectionString.txt");
+
+            DbContextOptions<siadbContext> options = new DbContextOptionsBuilder<siadbContext>().UseSqlServer(connectionString).Options;
+
+            siadbContext context = new siadbContext(options);
+
+            switch (menuString.ToLower())
+            {
+                case "main":
+                    return new MainMenu();
+                case "new-customer":
+                    return new NewCustomerMenu(new BL(new DBRepo(context)), new NewCustomerService());
+                case "customer":
+                    return new CustomerMenu(new BL(new DBRepo(context)), new CustomerService());
+                case "admin":
+                    return new AdminMenu(new BL(new DBRepo(context)), new AdminService());
+                default:
+                    return null;
+            }
+        }
     }
 }
